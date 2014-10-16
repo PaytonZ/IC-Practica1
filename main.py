c425a05640bc98f4857b0a8e0f4e3d5a3c242832
paytongod <juanlp01@ucm.es>
2014-10-16 19:43:37 +0200

    locuras hechas

--- main.py
+++ main.py
@@ -23,6 +23,7 @@ def qsort1(list):
         return []
     else:
         pivot = list[0]
+        print pivot
         lesser = qsort1([x for x in list[1:] if x < pivot])
         greater = qsort1([x for x in list[1:] if x >= pivot])
         return lesser + [pivot] + greater
